53d756f3779e25ef7b0b319b786ffdd111ee3f38
Chi Joel <abongsjoel@gmail.com>
2021-07-15 10:08:31 +0100

    Initial setup complete

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const el = screen.getByText(/hello/i);
+  expect(el).toBeInTheDocument();
 });
