83df1e79b1c767e76acc2dc553326544591f0115
Chi Joel <abongsjoel@gmail.com>
2021-07-20 11:19:05 +0100

    Redux setup complete

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,6 +1,12 @@
-import { combineReducers } from 'redux';
+import { combineReducers, createStore } from 'redux';
 import userEventsReducer from './user-events';
 
 const rootReducer = combineReducers({
   userEvents: userEventsReducer,
 });
+
+export type RootState = ReturnType<typeof rootReducer>;
+
+const store = createStore(rootReducer);
+
+export default store;
