ff7e98882182588b6022fb07ff0b387a463ade23
tj-ubuntu <abongsjoel@gmail.com>
2021-07-21 20:59:40 +0100

    Edit Event Titles

--- src/components/Calendar/EventItem.tsx
+++ src/components/Calendar/EventItem.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useDispatch } from 'react-redux';
 
-import { deleteUserEvent, UserEvent } from '../../redux/user-events';
+import {
+  deleteUserEvent,
+  updateUserEvent,
+  UserEvent,
+} from '../../redux/user-events';
 
 interface Props {
   event: UserEvent;
@@ -13,11 +17,56 @@ const EventItem: React.FC<Props> = ({ event }) => {
   const handleDeleteClick = () => {
     dispatch(deleteUserEvent(event.id));
   };
+
+  const [editable, setEditable] = useState(false);
+
+  const handleTitleClick = () => {
+    setEditable(true);
+  };
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (editable) {
+      inputRef.current?.focus();
+    }
+  }, [editable]);
+
+  const [title, setTitle] = useState(event.title);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+  };
+
+  const handleBlur = () => {
+    if (title !== event.title) {
+      dispatch(
+        updateUserEvent({
+          ...event,
+          title,
+        })
+      );
+    }
+    setEditable(false);
+  };
+
   return (
     <div key={event.id} className="calendar-event">
       <div className="calendar-event-info">
         <div className="calendar-event-time">10:00 - 12:00</div>
-        <div className="calendar-event-title">{event.title}</div>
+        <div className="calendar-event-title">
+          {editable ? (
+            <input
+              type="text"
+              ref={inputRef}
+              value={title}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+          ) : (
+            <span onClick={handleTitleClick}>{event.title}</span>
+          )}
+        </div>
       </div>
       <button
         className="calendar-event-delete-button"
--- src/redux/user-events.ts
+++ src/redux/user-events.ts
@@ -164,6 +164,55 @@ export const deleteUserEvent =
     }
   };
 
+const UPDATE_REQUEST = 'userEvents/update_request';
+
+interface UpdateRequstAction extends Action<typeof UPDATE_REQUEST> {}
+
+const UPDATE_SUCCESS = 'userEvents/update_success';
+
+interface UpdateSuccessAction extends Action<typeof UPDATE_SUCCESS> {
+  payload: { event: UserEvent };
+}
+
+const UPDATE_FAILURE = 'userEvents/update_failure';
+
+interface UpdateFailureAction extends Action<typeof UPDATE_FAILURE> {}
+
+export const updateUserEvent =
+  (
+    event: UserEvent
+  ): ThunkAction<
+    Promise<void>,
+    RootState,
+    undefined,
+    UpdateRequstAction | UpdateSuccessAction | UpdateFailureAction
+  > =>
+  async (dispatch) => {
+    dispatch({
+      type: UPDATE_REQUEST,
+    });
+
+    try {
+      const response = await fetch(`http://localhost:3001/events/${event.id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(event),
+      });
+      const updatedEvent: UserEvent = await response.json();
+
+      dispatch({
+        type: UPDATE_SUCCESS,
+        payload: { event: updatedEvent },
+      });
+    } catch (e) {
+      dispatch({
+        type: UPDATE_FAILURE,
+      });
+    }
+  };
+
 const selectUserEventState = (rootState: RootState) => rootState.userEvents;
 
 export const selectUserEventsArray = (rootState: RootState) => {
@@ -178,7 +227,11 @@ const initialState: UserEventsState = {
 
 const userEventsReducer = (
   state: UserEventsState = initialState,
-  action: LoadSuccessAction | CreateSuccessAction | DeleteSuccessAction
+  action:
+    | LoadSuccessAction
+    | CreateSuccessAction
+    | DeleteSuccessAction
+    | UpdateSuccessAction
 ) => {
   switch (action.type) {
     case LOAD_SUCCESS:
@@ -209,6 +262,13 @@ const userEventsReducer = (
       };
       delete newState.byIds[id];
       return newState;
+
+    case UPDATE_SUCCESS:
+      const { event: updatedEvent } = action.payload;
+      return {
+        ...state,
+        byIds: { ...state.byIds, [updatedEvent.id]: updatedEvent },
+      };
 
     default:
       return state;
