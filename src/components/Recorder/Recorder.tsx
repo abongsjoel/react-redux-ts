d3e09e2a1eda3a83729a7a1bfd18357127eac518
Chi Joel <abongsjoel@gmail.com>
2021-07-21 10:52:02 +0100

    Update timer

--- src/components/Recorder/Recorder.tsx
+++ src/components/Recorder/Recorder.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import cx from 'classnames';
 
 import { selectDateStart, start } from '../../redux/recorder';
 
@@ -9,19 +10,41 @@ const Recorder = () => {
   const dispatch = useDispatch();
   const dateStart = useSelector(selectDateStart);
   const started = dateStart !== '';
+  let interval = useRef<number>(0);
+  const [, setCount] = useState<number>(0);
 
   console.log({ dateStart });
 
   const handleClick = () => {
     dispatch(start());
+    interval.current = window.setInterval(() => {
+      setCount((count) => count + 1);
+    }, 1000);
   };
 
+  useEffect(() => {
+    return () => {
+      window.clearInterval(interval.current);
+    };
+  }, []);
+
+  let seconds = started
+    ? Math.floor((Date.now() - new Date(dateStart).getTime()) / 1000)
+    : 0;
+
+  const hours = seconds ? Math.floor(seconds / 60 / 60) : 0;
+  seconds -= hours * 60 * 60;
+  const minutes = seconds ? Math.floor(seconds / 60) : 0;
+  seconds -= minutes * 60;
+
   return (
-    <div className="recorder">
+    <div className={cx('recorder', { 'recorder-started': started })}>
       <button onClick={handleClick} className="recorder-record">
         <span></span>
       </button>
-      <div className="recorder-counter">00:00:00</div>
+      <div className="recorder-counter">
+        {hours}:{minutes}:{seconds}
+      </div>
     </div>
   );
 };
